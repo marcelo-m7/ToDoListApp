88de4962907783a8f7f45e3565c251aaad8adcb4
Marcelo Santos <marcelo.net77@hotmail.com>
2025-02-28 14:16:21 +0000

    feat: Filtering list items

--- todo.py
+++ todo.py
@@ -2,11 +2,15 @@ import flet as ft
 
 
 class Task(ft.Column):
-    def __init__(self, task_name, task_delete):
+    def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
+        self.completed = False
         self.task_name = task_name
+        self.task_status_change = task_status_change
         self.task_delete = task_delete
-        self.display_task = ft.Checkbox(value=False, label=self.task_name)
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
         self.edit_name = ft.TextField(expand=1)
 
         self.display_view = ft.Row(
@@ -18,12 +22,12 @@ class Task(ft.Column):
                     spacing=0,
                     controls=[
                         ft.IconButton(
-                            icon=ft.Icons.CREATE_OUTLINED,
+                            icon=ft.icons.CREATE_OUTLINED,
                             tooltip="Edit To-Do",
                             on_click=self.edit_clicked,
                         ),
                         ft.IconButton(
-                            ft.Icons.DELETE_OUTLINE,
+                            ft.icons.DELETE_OUTLINE,
                             tooltip="Delete To-Do",
                             on_click=self.delete_clicked,
                         ),
@@ -39,8 +43,8 @@ class Task(ft.Column):
             controls=[
                 self.edit_name,
                 ft.IconButton(
-                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
-                    icon_color=ft.Colors.GREEN,
+                    icon=ft.icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.colors.GREEN,
                     tooltip="Update To-Do",
                     on_click=self.save_clicked,
                 ),
@@ -60,6 +64,10 @@ class Task(ft.Column):
         self.edit_view.visible = False
         self.update()
 
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change()
+
     def delete_clicked(self, e):
         self.task_delete(self)
 
@@ -68,29 +76,56 @@ class TodoApp(ft.Column):
     # application's root control is a Column containing all other controls
     def __init__(self):
         super().__init__()
-        self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
         self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="active"), ft.Tab(text="completed")],
+        )
         self.width = 600
         self.controls = [
             ft.Row(
                 controls=[
                     self.new_task,
                     ft.FloatingActionButton(
-                        icon=ft.Icons.ADD, on_click=self.add_clicked
+                        icon=ft.icons.ADD, on_click=self.add_clicked
                     ),
                 ],
             ),
-            self.tasks,
+            ft.Column(
+                spacing=25,
+                controls=[
+                    self.filter,
+                    self.tasks,
+                ],
+            ),
         ]
 
     def add_clicked(self, e):
-        task = Task(self.new_task.value, self.task_delete)
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
         self.tasks.controls.append(task)
         self.new_task.value = ""
         self.update()
 
+    def task_status_change(self):
+        self.update()
+
     def task_delete(self, task):
         self.tasks.controls.remove(task)
+        self.update()
+
+    def before_update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+
+    def tabs_changed(self, e):
         self.update()
 
 
